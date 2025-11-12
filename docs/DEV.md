b2b1f2078a87fb06ece6d18e37a3a20d18dd59fa
kenwu <kenqiwu@gmail.com>
2025-11-12 11:37:54 -0800

    Add Sentry monitoring

---
+++ docs/DEV.md
@@ -0,0 +1,2 @@
+## Setting up Sentry with CI/CD
+Just make sure that the Sentry auth token from .env.sentry-build-plugin is available whenever you build and deploy your app.
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "ken-zw",
+    project: "javascript-react"
+  })],
   server: {
     port: 5173,
     open: true,
